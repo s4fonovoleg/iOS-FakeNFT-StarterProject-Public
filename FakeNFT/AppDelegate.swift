4a1a9199781c82f16c4f8b9e707c85bf9195cc7b
s4fonovoleg <s4fonovoleg@yandex.ru>
2024-02-11 19:51:38 +0300

    Revert "Merge branch 'Cart1-3' of https://github.com/s4fonovoleg/iOS-FakeNFT-StarterProject-Public into common"
    This reverts commit 300d7e2a2508cc8e023463b92ce7a8ce4a46611d, reversing
    changes made to cf5e2159b809fa538b2138dc55b536f31d28afa9.

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -2,22 +2,20 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
-  let servicesAssembly = ServicesAssembly(
-    networkClient: DefaultNetworkClient(),
-    nftStorage: NftStorageImpl()
-  )
-  let tabBarController = TabBarController()
-  
-  func application(
-    _: UIApplication,
-    didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    tabBarController.servicesAssembly = servicesAssembly
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = tabBarController
-    window?.makeKeyAndVisible()
-    
-    return true
-  }
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options _: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
 }
--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,41 +1,25 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-  // MARK: - Properties:
-  var servicesAssembly: ServicesAssembly!
-  
-  // MARK: - Private Properties:
-  private let profileNavigationViewController = UINavigationController(rootViewController: UIViewController())
-  private let catalogNavigationViewController = UINavigationController(rootViewController: UIViewController())
-  private let cartNavigationViewController = UINavigationController(rootViewController: CartViewController())
-  private let statisticsNavigationViewController = UINavigationController(rootViewController: UIViewController())
-  private let profileTabBarItem = UITabBarItem(title: L10n.Localizable.Button.profileTitle, image: UIImage(named: "profileIcon"), tag: 0)
-  private let catalogTabBarItem = UITabBarItem(title: L10n.Localizable.Button.catalogTitle, image: UIImage(named: "catalogIcon"), tag: 1)
-  private let cartTabBarItem = UITabBarItem(title: L10n.Localizable.Button.cartTitle, image: UIImage(named: "cartIcon"), tag: 2)
-  private let statisticsTabBarItem = UITabBarItem(title: L10n.Localizable.Button.statisticsTitle, image: UIImage(named: "statisticsIcon"), tag: 3)
-  
-  // MARK: - Private Methods:
-  private func setupTabs() {
-    profileNavigationViewController.tabBarItem = profileTabBarItem
-    catalogNavigationViewController.tabBarItem = catalogTabBarItem
-    cartNavigationViewController.tabBarItem = cartTabBarItem
-    statisticsNavigationViewController.tabBarItem = statisticsTabBarItem
-    
-    self.setViewControllers([
-      profileNavigationViewController,
-      catalogNavigationViewController,
-      cartNavigationViewController,
-      statisticsNavigationViewController
-    ], animated: true)
-  }
-}
 
-// MARK: - LifeCycle:
-extension TabBarController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    view.backgroundColor = .systemBackground
-    setupTabs()
-  }
+    var servicesAssembly: ServicesAssembly!
+
+    private let catalogTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.catalog", comment: ""),
+        image: UIImage(systemName: "square.stack.3d.up.fill"),
+        tag: 0
+    )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let catalogController = TestCatalogViewController(
+            servicesAssembly: servicesAssembly
+        )
+        catalogController.tabBarItem = catalogTabBarItem
+
+        viewControllers = [catalogController]
+
+        view.backgroundColor = .systemBackground
+    }
 }
