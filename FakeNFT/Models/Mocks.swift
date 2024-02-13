0e0a1215dd310de26618ec001ada18046993377c
ruslan0515015 <ruslaniosdev@gmail.com>
2024-02-13 19:15:24 +0300

    DeletenftViewController
    Configured DeleteNFTViewController's UI

--- FakeNFT/Models/Mocks.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import UIKit
-
-struct NFTMocks {
-  static var nfts: [NFTModel] = [
-    NFTModel(id: UUID(), name: "Zeus", image: UIImage(named: "ZeusIcon")!, price: "12,24")
-  ]
-}
--- Pods/Target Support Files/Pods-FakeNFT/Pods-FakeNFT-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FakeNFT : NSObject
-@end
-@implementation PodsDummy_Pods_FakeNFT
-@end
